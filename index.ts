0e216d9317773e8d4697300c06a749140189b77a
Michael Orenstein <michael@whiteshadows.me>
2019-06-07 14:56:56 +0930

    chore: Bumps

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 export {
   default as XboxOneControllerHintConsumerFactory,
 } from './src/hint-consumer'
+
 export { XboxOneControllerTransportFactory } from './src/transport-factory'
 export { default as HintValidatorXboxController } from './src/hint-validator'
